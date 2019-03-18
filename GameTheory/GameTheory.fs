7399d660fa8d22a2b3b2b158614e4bc52c669297
Quintisimo <quintuscardozo13@gmail.com>
2019-03-18 21:23:02 +1000

    started minmax implementation

---
+++ GameTheory/GameTheory.fs
@@ -0,0 +1,30 @@
+namespace QUT
+
+    module GameTheory =
+
+        let MiniMaxGenerator (heuristic:'Game -> 'Player -> int) (getTurn: 'Game -> 'Player) (gameOver:'Game->bool) (moveGenerator: 'Game->seq<'Move>) (applyMove: 'Game -> 'Move -> 'Game) : 'Game -> 'Player -> Option<'Move> * int =
+            // Basic MiniMax algorithm without using alpha beta pruning
+            let rec MiniMax (game: 'Game) (perspective: 'Player) =
+                NodeCounter.Increment()
+                let over = gameOver game
+                let isEven = NodeCounter.Count % 2 = 0
+
+                if over then
+                    (None, )
+                else
+                    let moves = moveGenerator game
+                    let newGameState = Seq.fold(fun gameState move -> (applyMove gameState move)) game moves
+                    let newPerspective = getTurn game
+
+                    MiniMax newGameState newPerspective
+            NodeCounter.Reset()
+            MiniMax
+
+        let MiniMaxWithAlphaBetaPruningGenerator (heuristic:'Game -> 'Player -> int) (getTurn: 'Game -> 'Player) (gameOver:'Game->bool) (moveGenerator: 'Game->seq<'Move>) (applyMove: 'Game -> 'Move -> 'Game) : int -> int -> 'Game -> 'Player -> Option<'Move> * int =
+            // Optimized MiniMax algorithm that uses alpha beta pruning to eliminate parts of the search tree that don't need to be explored            
+            let rec MiniMax alpha beta oldState perspective =
+                NodeCounter.Increment()
+                raise (System.NotImplementedException("Alpha Beta Pruning"))
+            NodeCounter.Reset()
+            MiniMax
+             
