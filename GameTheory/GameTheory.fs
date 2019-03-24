d63cef08408113c10c5884dd9e7bcdd06401da1b
Quintisimo <quintuscardozo13@gmail.com>
2019-03-24 13:17:07 +1000

    initial implementation of minimax with aplha beta

--- GameTheory/GameTheory.fs
+++ GameTheory/GameTheory.fs
@@ -32,9 +32,51 @@ namespace QUT
 
         let MiniMaxWithAlphaBetaPruningGenerator (heuristic:'Game -> 'Player -> int) (getTurn: 'Game -> 'Player) (gameOver:'Game->bool) (moveGenerator: 'Game->seq<'Move>) (applyMove: 'Game -> 'Move -> 'Game) : int -> int -> 'Game -> 'Player -> Option<'Move> * int =
             // Optimized MiniMax algorithm that uses alpha beta pruning to eliminate parts of the search tree that don't need to be explored            
-            let rec MiniMax alpha beta oldState perspective =
+            let rec MiniMax (alpha: int32) (beta: int32) (oldState: 'Game) (perspective: 'Player) =
                 NodeCounter.Increment()
-                raise (System.NotImplementedException("Alpha Beta Pruning"))
+                let over = gameOver oldState
+
+                if over then
+                    let score = heuristic oldState perspective
+                    (None, score)
+                else
+                    let moves = moveGenerator oldState
+                    let gameStates = Seq.map (fun move -> applyMove oldState move) moves
+                    let tuples = Seq.map (fun gameState -> MiniMax alpha beta gameState perspective) gameStates
+                    let scores = Seq.map (fun (move, score) -> score) tuples
+
+                    let nextPerspective = getTurn oldState
+
+                    if nextPerspective = perspective then
+                        let idealScore = Seq.max scores
+                        let newAlpha = match (idealScore > alpha) with
+                                       | true -> idealScore
+                                       | false -> alpha
+
+                        let idealTuple = Seq.find (fun (move, score) -> score = idealScore) tuples
+                        let newGameState = Seq.find (fun gameState -> MiniMax alpha beta oldState perspective = idealTuple) gameStates
+                        let idealMove = Seq.tryFind (fun move -> applyMove oldState move = newGameState) moves
+
+                        if beta <= newAlpha then
+                            (idealMove, idealScore)
+                        else
+                            MiniMax newAlpha beta newGameState perspective
+
+                    else
+                        let idealScore = Seq.min scores
+                        let newBeta = match (idealScore < beta) with
+                                      | true -> idealScore
+                                      | false -> beta
+
+                        let idealTuple = Seq.find (fun (move, score) -> score = idealScore) tuples
+                        let newGameState = Seq.find (fun gameState -> MiniMax alpha beta oldState perspective = idealTuple) gameStates
+                        let idealMove = Seq.tryFind (fun move -> applyMove oldState move = newGameState) moves
+
+
+                        if newBeta <= alpha then
+                           (idealMove, idealScore)
+                        else
+                           MiniMax alpha newBeta newGameState perspective
             NodeCounter.Reset()
             MiniMax
              
