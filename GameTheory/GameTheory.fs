e7b798f35477150c84126e2fee384404ec99998a
Quintisimo <quintuscardozo13@gmail.com>
2019-04-02 11:31:48 +1000

    converted sequence to list

--- GameTheory/GameTheory.fs
+++ GameTheory/GameTheory.fs
@@ -12,19 +12,19 @@ namespace QUT
                     let score = heuristic game perspective
                     (None, score)
                 else
-                    let moves = moveGenerator game
-                    let gameStates = Seq.map (fun move -> applyMove game move) moves
-                    let tuples = Seq.map (fun gameState -> MiniMax gameState perspective) gameStates
-                    let scores = Seq.map (fun (_, score) -> score) tuples
+                    let moves = Seq.toList (moveGenerator game)
+                    let gameStates = List.map (fun move -> applyMove game move) moves
+                    let tuples = List.map (fun gameState -> MiniMax gameState perspective) gameStates
+                    let scores = List.map (fun (_, score) -> score) tuples
 
                     let nextPerspective = getTurn game
                     let idealScore = match (nextPerspective = perspective) with
-                                     | true -> Seq.max scores
-                                     | false -> Seq.min scores
+                                     | true -> List.max scores
+                                     | false -> List.min scores
 
-                    let idealTuple = Seq.find (fun (_, score) -> score = idealScore) tuples
-                    let newGameState = Seq.find (fun gameState -> MiniMax gameState perspective = idealTuple) gameStates
-                    let idealMove = Seq.tryFind (fun move -> applyMove game move = newGameState) moves
+                    let idealTuple = List.find (fun (_, score) -> score = idealScore) tuples
+                    let newGameState = List.find (fun gameState -> MiniMax gameState perspective = idealTuple) gameStates
+                    let idealMove = List.tryFind (fun move -> applyMove game move = newGameState) moves
                     (idealMove, idealScore)               
             NodeCounter.Reset()
             MiniMax
@@ -39,34 +39,34 @@ namespace QUT
                     let score = heuristic oldState perspective
                     (None, score)
                 else
-                    let moves = moveGenerator oldState
-                    let gameStates = Seq.map (fun move -> applyMove oldState move) moves
-                    let tuples = Seq.map (fun gameState -> MiniMax alpha beta gameState perspective) gameStates
-                    let scores = Seq.map (fun (_, score) -> score) tuples
+                    let moves = Seq.toList (moveGenerator oldState)
+                    let gameStates = List.map (fun move -> applyMove oldState move) moves
+                    let tuples = List.map (fun gameState -> MiniMax alpha beta gameState perspective) gameStates
+                    let scores = List.map (fun (_, score) -> score) tuples
 
                     let nextPerspective = getTurn oldState
 
                     if nextPerspective = perspective then
-                        let idealScore = Seq.max scores
+                        let idealScore = List.max scores
                         let newAlpha = max idealScore alpha
 
                         if beta <= newAlpha then
                             (None, newAlpha)
                         else
-                            let idealTuple = Seq.find (fun (_, score) -> score = idealScore) tuples
-                            let newGameState = Seq.find (fun gameState -> MiniMax alpha beta gameState perspective = idealTuple) gameStates
-                            let idealMove = Seq.tryFind (fun move -> applyMove oldState move = newGameState) moves
+                            let idealTuple = List.find (fun (_, score) -> score = idealScore) tuples
+                            let newGameState = List.find (fun gameState -> MiniMax alpha beta gameState perspective = idealTuple) gameStates
+                            let idealMove = List.tryFind (fun move -> applyMove oldState move = newGameState) moves
                             (idealMove, newAlpha)
 
                     else
-                        let idealScore = Seq.min scores
+                        let idealScore = List.min scores
                         let newBeta = min idealScore beta
                         if newBeta <= alpha then
                             (None, newBeta)
                         else
-                            let idealTuple = Seq.find (fun (_, score) -> score = idealScore) tuples
-                            let newGameState = Seq.find (fun gameState -> MiniMax alpha beta gameState perspective = idealTuple) gameStates
-                            let idealMove = Seq.tryFind (fun move -> applyMove oldState move = newGameState) moves
+                            let idealTuple = List.find (fun (_, score) -> score = idealScore) tuples
+                            let newGameState = List.find (fun gameState -> MiniMax alpha beta gameState perspective = idealTuple) gameStates
+                            let idealMove = List.tryFind (fun move -> applyMove oldState move = newGameState) moves
                             (idealMove, newBeta)
             NodeCounter.Reset()
             MiniMax
